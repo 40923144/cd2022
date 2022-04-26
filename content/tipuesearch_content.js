8bb698f87d20ff105bf158d555cdbd3f8eade947
40923144 <40923144@gm.nfu.edu.tw>
2022-04-27 00:33:07 +0800

    倉儲連結

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': '關於', 'text': '\n', 'tags': '', 'url': '關於.html'}, {'title': 'w2', 'text': '16.子母件組合(零件) \n \n \n \n \n \n   \n \n \n \xa0 \n', 'tags': '', 'url': 'w2.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '網站倉儲: https://github.com/40923144/cd2022 \n \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'w2', 'text': '16.子母件組合(零件) \n \n \n \n \n \n   \n \n \n \xa0 \n', 'tags': '', 'url': 'w2.html'}]};
